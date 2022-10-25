20a0b0dc5197794095c9bba50e79b78d58781c76
Baharsah <me@baharsah.my.id>
2022-10-26 01:27:45 +0700

    Add React Babel and Webpack

---
+++ app/Interfaces/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Selamat!</h1>
+        <p>Aplikasi ReactIgniter telah terinstall</p>
+    </div>
+  );
+};
+
+export default App;
